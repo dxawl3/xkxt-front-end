4dabdde60314349c134ab35d6a1c866a36d7d0d0
dxawl3 <1044599282@qq.com>
2018-05-06 10:47:47 +0800

    完成：学生及老师个人设置修改密码模块

--- src/app/student-setting/student-setting.component.ts
+++ src/app/student-setting/student-setting.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {StudentService} from '../core/service/student.service';
+import {AppGlobalField} from '../core/config/app-global-field';
+import {Student} from '../core/domain/entity/student';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-student-setting',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentSettingComponent implements OnInit {
 
-  constructor() { }
+  showMessage: string;
+
+  constructor(private location: Location,
+              private route: Router,
+              private studentService: StudentService) {
+  }
 
   ngOnInit() {
   }
 
+  changePassword(oldPassword: string, newPassword: string, againPassword: string): void {
+    if (newPassword !== againPassword) {
+      this.showMessage = '【两次输入密码不一致】';
+    } else {
+      this.studentService.changePassword(
+        JSON.parse(localStorage.getItem(AppGlobalField.currentLoginUser)).studyNumber,
+        oldPassword, newPassword
+      ).subscribe(result => this.handleChangePassword(result));
+    }
+  }
+
+  private handleChangePassword(student: Student) {
+    if (student.studyNumber != null) {
+      // 密码修改成功
+      this.showMessage = '【密码修改成功！ 请重新登录】';
+      localStorage.clear();
+      setTimeout(() => this.route.navigateByUrl('/login'), 1000);
+    } else if (student.error != null) {
+      // 密码修改失败
+      this.showMessage = '【密码修改失败】' + student.message;
+    } else {
+      console.log(student);
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/teacher-setting/teacher-setting.component.ts
+++ src/app/teacher-setting/teacher-setting.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {TeacherService} from '../core/service/teacher.service';
+import {AppGlobalField} from '../core/config/app-global-field';
+import {Teacher} from '../core/domain/entity/teacher';
 
 @Component({
   selector: 'app-teacher-setting',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherSettingComponent implements OnInit {
 
-  constructor() { }
+  showMessage: string;
+
+  constructor(private route: Router,
+              private location: Location,
+              private teacherService: TeacherService) {
+  }
 
   ngOnInit() {
   }
 
+  changePassword(oldPassword: string, newPassword: string, againPassword: string): void {
+    if (newPassword !== againPassword) {
+      this.showMessage = '【两次输入密码不一致】';
+    } else {
+      this.teacherService.changePassword(
+        JSON.parse(localStorage.getItem(AppGlobalField.currentLoginUser)).teacherNumber,
+        oldPassword, newPassword
+      ).subscribe(result => this.handleChangePassword(result));
+    }
+  }
+
+  private handleChangePassword(teacher: Teacher) {
+    if (teacher.teacherNumber != null) {
+      // 密码修改成功
+      this.showMessage = '【密码修改成功！ 请重新登录】';
+      localStorage.clear();
+      setTimeout(() => this.route.navigateByUrl('/login'), 1000);
+    } else if (teacher.error != null) {
+      // 密码修改失败
+      this.showMessage = '【密码修改失败】' + teacher.message;
+    } else {
+      console.log(teacher);
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
