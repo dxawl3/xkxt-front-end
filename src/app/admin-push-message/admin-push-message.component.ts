e66d902d1a8e813102a3bd632a53bca0a2ff7ba7
dxawl3 <1044599282@qq.com>
2018-05-06 09:53:56 +0800

    完成：管理员发布公告模块

--- src/app/admin-push-message/admin-push-message.component.ts
+++ src/app/admin-push-message/admin-push-message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Message} from '../core/domain/entity/message';
+import {AdminService} from '../core/service/admin.service';
 
 @Component({
   selector: 'app-admin-push-message',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPushMessageComponent implements OnInit {
 
-  constructor() { }
+  showMessage: string;
+
+  constructor(private adminService: AdminService,
+              private location: Location) {
+  }
 
   ngOnInit() {
   }
 
+  pushMessage(title: string, content: string): void {
+    const message: Message = new Message(title, content);
+    this.adminService.pushMessage(message)
+      .subscribe(result => this.handlePushMessage(result));
+  }
+
+  private handlePushMessage(message: Message) {
+    if (message.title != null) {
+      // 发布成功
+      this.showMessage = '【公告发布成功】';
+      setTimeout(() => this.goBack(), 1000);
+    } else if (message.error != null) {
+      // 发布失败
+      this.showMessage = '【公告发布失败】' + message.message;
+    } else {
+      console.log(message);
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/core/domain/entity/message.ts
+++ src/app/core/domain/entity/message.ts
@@ -3,4 +3,10 @@ import {BaseEntity} from './base-entity';
 export class Message extends BaseEntity {
   title: string;
   content: string;
+
+  constructor(title: string, content: string) {
+    super();
+    this.title = title;
+    this.content = content;
+  }
 }
